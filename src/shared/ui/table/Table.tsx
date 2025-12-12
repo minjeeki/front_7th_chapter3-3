ac2df29cfeb4ef9909cf649c704c99aa1d614411
minjeeki <minjeeki@student.42seoul.kr>
2025-12-12 18:20:19 +0900

    fix: 빌드 에러 수정

--- src/shared/ui/table/Table.tsx
+++ src/shared/ui/table/Table.tsx
@@ -4,42 +4,58 @@ export interface TableProps extends React.HTMLAttributes<HTMLTableElement> {
   className?: string
 }
 
+export interface TableSectionProps extends React.HTMLAttributes<HTMLTableSectionElement> {
+  className?: string
+}
+
+export interface TableRowProps extends React.HTMLAttributes<HTMLTableRowElement> {
+  className?: string
+}
+
+export interface TableCellProps extends React.TdHTMLAttributes<HTMLTableCellElement> {
+  className?: string
+}
+
+export interface TableHeadProps extends React.ThHTMLAttributes<HTMLTableCellElement> {
+  className?: string
+}
+
 export const Table = forwardRef<HTMLTableElement, TableProps>(({ className, ...props }, ref) => (
   <div className="w-full overflow-auto">
-    <table ref={ref} className={`table-fixed w-full caption-bottom text-sm ${className}`} {...props} />
+    <table ref={ref} className={`table-fixed w-full caption-bottom text-sm ${className || ''}`} {...props} />
   </div>
 ))
 Table.displayName = "Table"
 
-export const TableHeader = forwardRef<HTMLTableSectionElement, TableProps>(({ className, ...props }, ref) => (
-  <thead ref={ref} className={`[&_tr]:border-b ${className}`} {...props} />
+export const TableHeader = forwardRef<HTMLTableSectionElement, TableSectionProps>(({ className, ...props }, ref) => (
+  <thead ref={ref} className={`[&_tr]:border-b ${className || ''}`} {...props} />
 ))
 TableHeader.displayName = "TableHeader"
 
-export const TableBody = forwardRef<HTMLTableSectionElement, TableProps>(({ className, ...props }, ref) => (
-  <tbody ref={ref} className={`[&_tr:last-child]:border-0 ${className}`} {...props} />
+export const TableBody = forwardRef<HTMLTableSectionElement, TableSectionProps>(({ className, ...props }, ref) => (
+  <tbody ref={ref} className={`[&_tr:last-child]:border-0 ${className || ''}`} {...props} />
 ))
 TableBody.displayName = "TableBody"
 
-export const TableRow = forwardRef<HTMLTableRowElement, TableProps>(({ className, ...props }, ref) => (
+export const TableRow = forwardRef<HTMLTableRowElement, TableRowProps>(({ className, ...props }, ref) => (
   <tr
     ref={ref}
-    className={`border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted h-14 ${className}`}
+    className={`border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted h-14 ${className || ''}`}
     {...props}
   />
 ))
 TableRow.displayName = "TableRow"
 
-export const TableHead = forwardRef<HTMLTableCellElement, TableProps>(({ className, ...props }, ref) => (
+export const TableHead = forwardRef<HTMLTableCellElement, TableHeadProps>(({ className, ...props }, ref) => (
   <th
     ref={ref}
-    className={`h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 ${className}`}
+    className={`h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 ${className || ''}`}
     {...props}
   />
 ))
 TableHead.displayName = "TableHead"
 
-export const TableCell = forwardRef<HTMLTableCellElement, TableProps>(({ className, ...props }, ref) => (
-  <td ref={ref} className={`p-2 align-middle [&:has([role=checkbox])]:pr-0 ${className}`} {...props} />
+export const TableCell = forwardRef<HTMLTableCellElement, TableCellProps>(({ className, ...props }, ref) => (
+  <td ref={ref} className={`p-2 align-middle [&:has([role=checkbox])]:pr-0 ${className || ''}`} {...props} />
 ))
 TableCell.displayName = "TableCell"
